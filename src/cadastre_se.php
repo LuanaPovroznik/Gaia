7dff1e5187c37c9aceaa7be08578de15022e495f
Matheus Portes <matheusportes12@gmail.com>
2022-06-05 16:36:24 -0300

    feat: nova index e tela de cadastro finalizadas

---
+++ src/cadastre_se.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/cadastre_se_style.css">
+    <title>Início</title>
+</head>
+<body>
+<div class="container">
+    <div class="center">
+        <div class="right">
+            <form action="#">
+                <h2 style="font-family: 'Asap Condensed Medium'; font-weight: normal">Cadastre-se</h2>
+                <input type="text" placeholder="Nome completo">
+                <br><br>
+                <input type="password" placeholder="Senha">
+                <br> <br>
+                <input type="text" placeholder="CPF">
+                <br><br>
+                <input type="text" placeholder="E-mail">
+                <br><br>
+                <input type="text" placeholder="Nome de usuário">
+                <br><br>
+                <input type="submit" value="Cadastre-se" class="loginButton">
+                <a href="index2.php"><p class="registerLink">Já possui uma conta? Faça login</p></a>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ src/index2.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/index_style2.css">
+    <title>Início</title>
+</head>
+<body>
+<div class="container">
+    <div class="center">
+        <div class="left">
+            <h3>SISTEMA GAIA</h3>
+            <span>
+        Buscando facilitar a rotina de advogados no
+    </span><br>
+            <span>Brasil. O seu assistente para gestão </span><br>
+            <span>
+                jurídica de qualidade.
+            </span>
+            <br>
+            <h4>Comece agora</h4>
+            <span>
+            Entre em contato e realize um orçamento para<br>
+        </span>
+            <span>
+            implementação do Gaia em seu escritório.
+        </span>
+            <br><br>
+            <a href="https://unicuritiba.com.br" target="_blank">
+                <button type="button" class="getStarted">
+                    Orçamento
+                </button>
+            </a>
+        </div>
+
+        <div class="right">
+            <form action="#">
+                <h2 style="font-family: 'Asap Condensed Medium'; font-weight: normal">Entre na sua conta</h2>
+                <input type="text" placeholder="Nome de usuário ou e-mail">
+                <br><br>
+                <input type="password" placeholder="Senha">
+                <br> <br>
+<!--                <input type="submit" value="Cadastre-se" class="registerButton">-->
+                <input type="submit" value="Login" class="loginButton">
+                <a href="cadastre_se.php"><p class="registerLink">Não possui conta? Cadastre-se</p></a>
+            </form>
+            <footer style="margin-top: 45%; font-size: small; font-family: Montserrat, sans-serif; color: darkgrey"><h4>Luana Povroznik & Matheus Portes</h4></footer>
+        </div>
+    </div>
+</div>
+</body>
+</html>
